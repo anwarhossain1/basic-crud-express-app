611ffd06771325930bc1b803efbe91148afff050
Anwar Hossain <irishkhan33@gmail.com>
2024-04-30 17:08:27 +0600

    joi added for custom validation

--- simple-crud.request.js
+++ simple-crud.request.js
@@ -2,8 +2,8 @@ const Joi = require('joi')
 
 const schema = Joi.object({
     name: Joi.string().min(3).max(10).required(),
-    age: Joi.number().required(),
-})
+    age: Joi.number().integer().required(),
+}).options({ convert: false });
 
 const validate = (data)=>{
     const validationResult = schema.validate(data)
